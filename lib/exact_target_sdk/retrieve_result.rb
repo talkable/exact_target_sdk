b6b07cb647845bc484e56c32712a176b553121f5
Ivan Arakcheev <wildfiler0@gmail.com>
2015-08-17 18:35:30 +0300

    Create methods for properties in RetrievResult

--- lib/exact_target_sdk/retrieve_result.rb
+++ lib/exact_target_sdk/retrieve_result.rb
@@ -1,4 +1,17 @@
 module ExactTargetSDK
 class RetrieveResult < Result
+  def initialize(result)
+    super(result)
+    return if @result[:properties].nil?
+    property_array = @result[:properties][:property]
+
+    property_array = [property_array] if property_array.is_a?(Hash)
+
+    property_array.each do |property|
+      define_singleton_method property[:name] do
+        property[:value]
+      end
+    end
+  end
 end
 end
